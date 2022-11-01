0ad4f9e3e3752ba15ccbf3b78b95afd2d62b58b0
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-01 20:42:55 +0530

    Update InputQuery.js
    exported

--- src/components/InputQuery.js
+++ src/components/InputQuery.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import App from './App';
 
 function InputQuery() {
     return (
         <input placeHolder={'Enter your query here..'} />
     )
 }
+export default InputQuery;
