fe05463864efd9ed41a9b8f61ae5b9dcf3126c2e
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-01 20:44:52 +0530

    Update SubmitButton.js
    exported

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import App from './App';
 
 function SubmitButton() {
     return (
         <button>Ask</button>     
     )
 }
+export default SubmitButton;
