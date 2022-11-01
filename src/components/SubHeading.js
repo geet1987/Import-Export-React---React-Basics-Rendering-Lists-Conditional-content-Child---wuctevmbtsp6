25cbfbfa4da7a3bdb0a8824f3a16dc8173afc347
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-01 20:43:54 +0530

    Update SubHeading.js

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import App from './App';
+
 
 function SubHeading() {
     return (
         <h5>Happy to solve you doubts.</h5>
     )
 }
+export default SubHeading;
