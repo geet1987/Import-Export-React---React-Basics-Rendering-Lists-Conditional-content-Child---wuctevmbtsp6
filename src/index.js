3538ac4f6fbc25a3aabc016f8ffd65bc7dd170ab
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-02 17:49:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Heading from "./components";
+import InputQuery from "./components";
+import SubHeading from "./components";
+import SubmitButton from "./components";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+export {Heading, InputQuery, SubHeading, SubmitButton};
